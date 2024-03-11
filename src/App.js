34b8ffdd4f0277445d3a4d62c5acaf533e259682
Gbessaya-joseph <josephgbessaya@gmail.com>
2024-03-11 23:18:17 +0000

    Deuxième version de ma formation React

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 
 const title = 'Bonjour les gens'
-function App() {
 const showTitle = true
-const handClick = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    console.log(e.target)
-    alert("Je suis une alerte")
-}
-  return  <div className="App">
-    {showTitle && <h1 onClick={handClick} id="hello-world" className="hello-world" style={{color: 'red',
-     background: 'blue', cursor: 'pointer'}}>{title}</h1>}
+const todo = ['bois', 'pierre', 'feuille', 'can']
+function App() {
+  const handClick = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        console.log(e.target)
+        alert("Je suis une alerte")
+  }
+  return  <>
+    {showTitle ? <h1 onClick={handClick} id="hello-world" className="hello-world" style={{color: 'red',
+     background: 'blue', cursor: 'pointer'}}>{title}</h1> : null}
     <p>lorem</p>
-
-  </div>
+    <ul>
+        {todo.map((t, index) => <li key={index}>{t}</li>)}
+    </ul>
+   <Title color="blue" hidden={false}>Hello World</Title>
+  </>
 
 }
 
+function Title({color, children, hidden}) {
+
+    if (hidden) {
+        return null
+    }
+    const propos = {
+    id: "monid",
+    className: "maclass"
+    }
+
+    return <h1 style = {{color: color}}>{children}</h1>
+
+}
 export default App;
